eed57b2e0d9e3055a7a05a5080f6710603c0b1ed
1y29h <1y29h0.0@gmail.com>
2025-10-27 04:39:23 +0900

    프로젝트 초기 업로드

---
+++ FE/zzaptalk-frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    global: "window",
+  },
+  server: {
+    host: true,
+    allowedHosts: [".ngrok-free.dev"],
+    hmr: {
+      clientPort: 443,
+    },
+  },
+});
