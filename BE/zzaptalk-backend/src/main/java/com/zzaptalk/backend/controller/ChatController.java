addedbb07b6d4cabe37ab418c296daf18762e46d
1y29h <1y29h0.0@gmail.com>
2025-11-02 20:14:26 +0900

    .gitignore 수정

--- BE/zzaptalk-backend/src/main/java/com/zzaptalk/backend/controller/ChatController.java
+++ BE/zzaptalk-backend/src/main/java/com/zzaptalk/backend/controller/ChatController.java
@@ -6,16 +6,9 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-/**
- * 클라이언트가 /app/sendMessage 로 메시지를 보내면
- * 서버가 /topic/public 으로 다시 뿌려줌
- */
-
 @Controller
 public class ChatController {
 
-    // ex) 클라이언트가 /app/chat.sendMessage.room1 으로 보내면
-    // 서버는 /topic/chat.room1 구독자들에게 전송
     @MessageMapping("/chat.sendMessage.{roomId}")
     @SendTo("/topic/chat.{roomId}")
     public ChatMessage sendMessage(@DestinationVariable String roomId, ChatMessage message) {
