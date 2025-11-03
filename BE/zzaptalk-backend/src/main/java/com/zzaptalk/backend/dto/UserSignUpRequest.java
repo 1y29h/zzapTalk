9d00bf0921daa894a8eb4567639a90b0569c13ed
1y29h <1y29h0.0@gmail.com>
2025-11-03 22:54:41 +0900

    dto 작성

--- BE/zzaptalk-backend/src/main/java/com/zzaptalk/backend/dto/UserSignUpRequest.java
+++ BE/zzaptalk-backend/src/main/java/com/zzaptalk/backend/dto/UserSignUpRequest.java
@@ -1,2 +1,24 @@
-package com.zzaptalk.backend.dto;public class UserSignUpRequest {
+package com.zzaptalk.backend.dto;
+
+import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSignUpRequest {
+
+    @NotBlank private String phoneNum;
+    @Email @NotBlank private String email;
+    @NotBlank private String pwd;
+    @NotBlank private String name;
+    @NotBlank private String nickname;
+    @NotBlank private String RRN;
+    private String zzapID;
+
 }
