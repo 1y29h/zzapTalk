8d7810e24fbb89b5611ee510114e77c312fd7ebc
minseo <gimminseo288@gamil.com>
2025-11-09 00:01:24 +0900

    아이콘 깨짐 현상 고치기

--- FE/zzaptalk-front/app/_layout.tsx
+++ FE/zzaptalk-front/app/_layout.tsx
@@ -1,23 +1,13 @@
 import { Stack } from "expo-router";
-import { Helmet } from "react-helmet"; // ✅ 추가 설치 필요
+import { Helmet } from "react-helmet";
 
 export default function RootLayout() {
   return (
     <>
-      {/* Helmet으로 head 영역 수정 */}
       <Helmet>
-        {/* expo-vector-icons 웹용 폰트 경로 추가 */}
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/@expo/vector-icons@13.0.0/build/vendor/react-native-vector-icons/Fonts/Ionicons.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/@expo/vector-icons@13.0.0/build/vendor/react-native-vector-icons/Fonts/Feather.css"
-        />
+        <link rel="stylesheet" href="/global-icons.css" />
         <link rel="icon" href="/favicon.png" />
       </Helmet>
-
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
         <Stack.Screen name="signup" />
