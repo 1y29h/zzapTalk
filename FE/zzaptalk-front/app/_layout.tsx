1732a8aa80bdcbf0df092ce00a218a96b1f0a299
1y29h <1y29h0.0@gmail.com>
2025-11-08 23:42:38 +0900

    채팅화면 아이콘 깨짐 해결

--- FE/zzaptalk-front/app/_layout.tsx
+++ FE/zzaptalk-front/app/_layout.tsx
@@ -1,10 +1,27 @@
 import { Stack } from "expo-router";
+import { Helmet } from "react-helmet"; // ✅ 추가 설치 필요
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="signup" />
-    </Stack>
+    <>
+      {/* Helmet으로 head 영역 수정 */}
+      <Helmet>
+        {/* expo-vector-icons 웹용 폰트 경로 추가 */}
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/@expo/vector-icons@13.0.0/build/vendor/react-native-vector-icons/Fonts/Ionicons.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/@expo/vector-icons@13.0.0/build/vendor/react-native-vector-icons/Fonts/Feather.css"
+        />
+        <link rel="icon" href="/favicon.png" />
+      </Helmet>
+
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="signup" />
+      </Stack>
+    </>
   );
 }
