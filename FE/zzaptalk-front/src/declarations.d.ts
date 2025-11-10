ae406dcee700ab893d5fd94e32fe2cd6f3bc355d
1y29h <1y29h0.0@gmail.com>
2025-11-11 01:03:45 +0900

    채팅 목록/ 화면

--- FE/zzaptalk-front/src/declarations.d.ts
+++ FE/zzaptalk-front/src/declarations.d.ts
@@ -1,5 +1,8 @@
-// declarations.d.ts
-declare module "*.module.css" {
-  const classes: { [key: string]: string };
-  export default classes;
+// src/declarations.d.ts
+
+// React Native의 .module 스타일 파일을 StyleSheet 객체로 인식시킴
+declare module "*.module" {
+  import { StyleSheet } from "react-native";
+  const styles: ReturnType<typeof StyleSheet.create>;
+  export default styles;
 }
