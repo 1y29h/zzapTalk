fb3e76c3f475adcd0c772bba132b9a5beff57e00
Yeon <1y29h0.0@gmail.com>
2025-11-10 17:01:19 +0900

    주석 이모티콘 제거

--- BE/zzaptalk-backend/src/main/java/com/zzaptalk/backend/config/WebSocketConfig.java
+++ BE/zzaptalk-backend/src/main/java/com/zzaptalk/backend/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")  // ✅ 모든 origin 허용
+                .setAllowedOriginPatterns("*")  // 모든 origin 허용
                 .withSockJS();
     }
 
