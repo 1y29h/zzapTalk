756b48ca0b3d2fd1abf5d0788e8a6d1df7325699
Yeon <1y29h0.0@gmail.com>
2025-11-10 11:31:02 +0900

    로그인 레포 작성

--- BE/zzaptalk-backend/src/main/java/com/zzaptalk/backend/repository/UserRepository.java
+++ BE/zzaptalk-backend/src/main/java/com/zzaptalk/backend/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.zzaptalk.backend.repository;
 import com.zzaptalk.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     // -------------------------------------------------------------------------
@@ -11,5 +13,19 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     // 전화번호 중복 체크
     boolean existsByPhoneNum(String phoneNum);
+
+    // -------------------------------------------------------------------------
+    // 로그인
+    // -------------------------------------------------------------------------
+
+    // 전화번호로 사용자 찾기
+    // 조회 실패 가능성 때문에 Optional 사용
+    Optional<User> findByPhoneNum(String phoneNum);
+
+    // 이메일로 사용자 찾기
+    Optional<User> findByEmail(String email);
+
+    // ZzapTalk ID로 사용자 찾기
+    Optional<User> findByZzapID(String zzapID);
 
 }
