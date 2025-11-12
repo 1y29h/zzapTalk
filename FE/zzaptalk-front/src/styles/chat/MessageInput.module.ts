098d158be010c32314585a4e0ed23bbe048db743
minseo <gimminseo288@gamil.com>
2025-11-12 17:32:45 +0900

    디자인 수정

--- FE/zzaptalk-front/src/styles/chat/MessageInput.module.ts
+++ FE/zzaptalk-front/src/styles/chat/MessageInput.module.ts
@@ -4,14 +4,13 @@ export default StyleSheet.create({
   container: {
     flexDirection: "row",
     alignItems: "center",
-    gap: 10,
+    columnGap: 10,
     paddingHorizontal: 12,
     paddingVertical: 10,
     borderTopWidth: 1,
     borderColor: "#EDEDED",
     backgroundColor: "#fff",
   },
-
   circleBtn: {
     width: 40,
     height: 40,
@@ -20,24 +19,21 @@ export default StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-
   inputWrap: {
     flex: 1,
     position: "relative",
     backgroundColor: "#F3F3F5",
     borderRadius: 22,
-    minHeight: 46, // 입력창 높이
+    minHeight: 46,
     justifyContent: "center",
   },
-
   input: {
     paddingLeft: 14,
-    paddingRight: 56, // 버튼 공간 확보
+    paddingRight: 56,
     paddingVertical: 10,
     fontSize: 14,
     maxHeight: 120,
   },
-
   sendFab: {
     position: "absolute",
     right: 8,
